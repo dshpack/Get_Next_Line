1c2322fd926165dced31dd5600a6f17ed87cbb6c
dshpack starling <shpackdimon@ukr.net>
2019-04-11 03:04:34 +0300

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshpack <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/16 22:20:51 by dshpack           #+#    #+#             */
+/*   Updated: 2018/06/16 22:21:04 by dshpack          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# define BUFF_SIZE 1
+
+int			get_next_line(const int fd, char **line);
+
+#endif
